2c8a463fda77f695bddee62508da48e54bc2f94a
Simón Acosta <simonacostfc@gmail.com>
2025-10-28 18:23:58 -0500

    routers del main

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 from fastapi import FastAPI
+import Categorias
+import Productos
+from db import create_tables
 
+app = FastAPI(lifespan=create_tables, title="Tienda API")
+app.include_router(Categorias.router, tags=["Categorias"], prefix="/categorias")
+app.include_router(Productos.router, tags=["Productos"], prefix="/productos")
