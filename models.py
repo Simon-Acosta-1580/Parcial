26464d1438a1d1924f42f5c75d851406925cf2c5
Simón Acosta <simonacostfc@gmail.com>
2025-10-28 17:30:39 -0500

    relaciones entre modelos

--- models.py
+++ models.py
@@ -4,9 +4,16 @@ class CategoriaBase(SQLModel):
     nombre: str | None = Field(description="Nombre de la categoria")
     descripcion: str | None = Field(description="Descripcion de la categoria")
 
-class ProductosBase(SQLModel):
+class ProductoBase(SQLModel):
     nombre: str | None = Field(description="Nombre de la producto")
     precio: float | None = Field(description="Precio de la producto")
     stock: float | None = Field(description="Stock de la producto")
     descripcion: str | None = Field(description="Descripcion de la producto")
 
+class Categorias(CategoriaBase, Table= True):
+    id: int | None = Field(default = None, primary_key=True)
+    productos: list["Producto"] = Relationship(back_populates="categoria")
+
+class Producto(ProductoBase, Table= True):
+    id: int | None = Field(default = None, primary_key=True)
+    categoria: Categorias = Relationship(back_populates="productos")
