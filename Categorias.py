5aecb364ed860e4981a21adc4ca2deee469d2780
Simón Acosta <simonacostfc@gmail.com>
2025-10-28 18:33:39 -0500

    obtener categoria y sus productos, correccion logica de negocios en el create_producto

--- Categorias.py
+++ Categorias.py
@@ -1,5 +1,5 @@
 from db import SessionDep
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from models import Categoria, CategoriaBase
 from sqlmodel import select
 
@@ -7,6 +7,12 @@ router = APIRouter()
 
 @router.post("/", response_model=Categoria, status_code=201)
 async def create_categoria(nueva_categoria: CategoriaBase, session: SessionDep):
+    query = select(Categoria).where(Categoria.nombre == nueva_categoria.nombre)
+    existente = session.exec(query).first()
+
+    if existente:
+        raise HTTPException(status_code=400, detail="Ya existe una categoría con ese nombre.")
+
     categoria = Categoria.model_validate(nueva_categoria)
     session.add(categoria)
     session.commit()
@@ -18,3 +24,15 @@ async def listar_categorias_activas(session: SessionDep):
     query = select(Categoria).where(Categoria.status == True)
     categorias = session.exec(query).all()
     return categorias
+
+@router.get("/{categoria_id}", response_model=Categoria)
+async def obtener_categoria_con_productos(categoria_id: int, session: SessionDep):
+    query = select(Categoria).where(Categoria.id == categoria_id)
+    categoria = session.exec(query).first()
+
+    if not categoria:
+        raise HTTPException(status_code=404, detail="Categoría no encontrada")
+
+    _ = categoria.productos
+
+    return categoria
--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from db import create_tables
 
 app = FastAPI(lifespan=create_tables, title="Tienda API")
 app.include_router(Categorias.router, tags=["Categorias"], prefix="/categorias")
-app.include_router(Productos.router, tags=["Productos"], prefix="/productos")
+
